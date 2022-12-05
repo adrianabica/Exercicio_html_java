9de9e6ad586853a1cfac0708d045e091362ce526
Adriana Bica <contato@systemweb.tec.br>
2022-12-05 07:45:01 -0300

    commit Java

---
+++ main.js
@@ -0,0 +1,13 @@
+const validaValorA = document.getElementById('campo-A');
+const validaValorB = document.getElementById('campo-B');
+
+function validaValor(' '); {
+    formValido = validavalor(validaValorA.value > validaValorB.value);
+    if (validaValorA.value > validaValorB.value) {
+        alert ('Confirmado');
+    }
+
+    else {
+        alert('Recusado');
+    }
+})
